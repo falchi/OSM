5090c0b8e1afeaa1856988c00c88448ffe42bedc
Herman Falchi Gálvez <herman.falchi@alumnos.usm.cl>
2012-12-08 03:45:21 -0300

    First map

--- OSM/src/falchi/ej/osmdroid/MainActivity.java
+++ OSM/src/falchi/ej/osmdroid/MainActivity.java
@@ -3,13 +3,23 @@ package falchi.ej.osmdroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MyLocationOverlay;
 
 public class MainActivity extends Activity {
+	
+	private MapView myOpenMapView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        myOpenMapView = (MapView)findViewById(R.id.openmapview);
+        myOpenMapView.setBuiltInZoomControls(true);
+        MapController myMapController = myOpenMapView.getController();
+        myMapController.setZoom(4);
     }
 
     @Override
